c52f83c1aa09557f000c23d26b037428243f8cfc
zyzhuaidan <1963806558@qq.com>
2023-02-08 23:15:09 +0800

    init

---
+++ src/test/java/com/xingzhou/springboot_quartz/SpringbootQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xingzhou.springboot_quartz;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootQuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
